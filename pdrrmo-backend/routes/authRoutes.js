e7e538dbd2d4441070ded7dfc9a1e0d1d3b2aa2b
James Nicolas <jpatricknicolas@gmail.com>
2025-06-25 10:34:22 +0800

    Fixed user on authRoutes

--- pdrrmo-backend/routes/authRoutes.js
+++ pdrrmo-backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { loginUser, registerUser } = require("../controllers/authController");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const router = express.Router();
 
